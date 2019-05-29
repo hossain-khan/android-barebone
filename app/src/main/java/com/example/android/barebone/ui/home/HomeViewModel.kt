8f4465bb88f6d78751774d630bae8dc7c36f2b88
Hossain Khan <amardeshbd@gmail.com>
2019-05-29 15:10:23 -0400

    [ADDED] Databinding and navigation event.

--- app/src/main/java/com/example/android/barebone/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/android/barebone/ui/home/HomeViewModel.kt
@@ -1,12 +1,28 @@
 package com.example.android.barebone.ui.home
 
 import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.barebone.ui.extensions.LiveEvent
 import timber.log.Timber
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(private val preferences: SharedPreferences) : ViewModel() {
+
+    private val _featureXEvent = LiveEvent<Unit>()
+    private val _featureZEvent = LiveEvent<Unit>()
+    val featureXEvent: LiveData<Unit> = _featureXEvent
+    val featureZEvent: LiveData<Unit> = _featureZEvent
+
     init {
         Timber.d("Got injected preference: $preferences")
+    }
+
+    fun openFeatureXClicked() {
+        _featureXEvent.value = Unit
+    }
+
+    fun openFeatureZClicked() {
+        _featureZEvent.value = Unit
     }
 }
