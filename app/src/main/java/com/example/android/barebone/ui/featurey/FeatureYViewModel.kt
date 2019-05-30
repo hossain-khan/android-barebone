bbd8dca40451f3e4eaafcc6df1f670b51e45b152
Hossain Khan <amardeshbd@gmail.com>
2019-05-29 22:00:10 -0400

    [ADDED] Adapter that used data bound adapter
    Fixes #13

--- app/src/main/java/com/example/android/barebone/ui/featurey/FeatureYViewModel.kt
+++ app/src/main/java/com/example/android/barebone/ui/featurey/FeatureYViewModel.kt
@@ -1,6 +1,30 @@
 package com.example.android.barebone.ui.featurey
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.barebone.ui.featurey.model.ItemModel
 import javax.inject.Inject
 
-class FeatureYViewModel @Inject constructor() : ViewModel()
+class FeatureYViewModel @Inject constructor() : ViewModel() {
+    private val _data = MutableLiveData<List<ItemModel>>()
+    val data: LiveData<List<ItemModel>> = _data
+
+    private val sampleData = mutableListOf<ItemModel>()
+
+    init {
+        generateSampleDataSet()
+        _data.value = sampleData.toList()
+    }
+
+    private fun generateSampleDataSet() {
+        for (i in 1..20) {
+            sampleData.add(ItemModel(i, "This is sample item with id: $i"))
+        }
+    }
+
+    fun onItemClicked(item: ItemModel) {
+        sampleData.remove(item)
+        _data.value = sampleData.toList()
+    }
+}
