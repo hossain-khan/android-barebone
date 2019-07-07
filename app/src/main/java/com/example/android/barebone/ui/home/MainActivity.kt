191c2393862dccf1d182fedfc99734696a452f74
Hossain Khan <amardeshbd@gmail.com>
2019-07-06 20:59:08 -0400

    [ADDED] View pager for the main screen bottom navigation view
    Most of the code example is taken from https://pspdfkit.com/blog/2019/using-the-bottom-navigation-view-in-android/

--- app/src/main/java/com/example/android/barebone/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/android/barebone/ui/home/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.android.barebone.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
 import com.example.android.barebone.R
 import com.example.android.barebone.databinding.ActivityMainBinding
 import com.example.android.barebone.ui.featurex.FeatureXActivity
@@ -17,31 +20,14 @@ import dagger.android.AndroidInjection
 import timber.log.Timber
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var viewModel: HomeViewModel
     private lateinit var binding: ActivityMainBinding
-
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                binding.message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                binding.message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                binding.message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    private lateinit var mainPagerAdapter: MainPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -54,9 +40,37 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel = viewModel
 
 
-        binding.navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        setupBottomNavigationBar()
 
         observeNavigationEvents(viewModel)
+    }
+
+    private fun setupBottomNavigationBar() {
+        // Initialize components/views.
+        mainPagerAdapter = MainPagerAdapter(supportFragmentManager)
+
+        // Show the default screen.
+        selectScreen(MainScreen.HOME)
+
+        // Set the listener for item selection in the bottom navigation view.
+        binding.navView.setOnNavigationItemSelectedListener(this)
+
+        // Attach an adapter to the view pager and make it select the bottom navigation
+        // menu item and change the title to proper values when selected.
+        binding.viewPager.adapter = mainPagerAdapter
+        binding.viewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
+            override fun onPageSelected(position: Int) {
+                val selectedScreen = mainPagerAdapter.getItems()[position]
+                selectBottomNavigationViewMenuItem(selectedScreen.menuItemId)
+                supportActionBar?.setTitle(selectedScreen.titleStringId)
+            }
+        })
+    }
+
+    private fun selectScreen(defaultScreen: MainScreen) {
+        scrollToScreen(defaultScreen)
+        selectBottomNavigationViewMenuItem(defaultScreen.menuItemId)
+        supportActionBar?.setTitle(defaultScreen.titleStringId)
     }
 
     /**
@@ -77,5 +91,36 @@ class MainActivity : AppCompatActivity() {
             Timber.i("Launching feature Z activity.")
             startActivity(Intent(this@MainActivity, FeatureZActivity::class.java))
         })
+    }
+
+    /**
+     * Scrolls ViewPager to show the provided screen.
+     */
+    private fun scrollToScreen(mainScreen: MainScreen) {
+        val screenPosition = mainPagerAdapter.getItems().indexOf(mainScreen)
+        if (screenPosition != binding.viewPager.currentItem) {
+            binding.viewPager.currentItem = screenPosition
+        }
+    }
+
+    /**
+     * Selects the specified item in the bottom navigation view.
+     */
+    private fun selectBottomNavigationViewMenuItem(@IdRes menuItemId: Int) {
+        binding.navView.setOnNavigationItemSelectedListener(null)
+        binding.navView.selectedItemId = menuItemId
+        binding.navView.setOnNavigationItemSelectedListener(this)
+    }
+
+    /**
+     * Listener implementation for registering bottom navigation clicks.
+     */
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        getMainScreenForMenuItem(menuItem.itemId)?.let { screenItem ->
+            scrollToScreen(screenItem)
+            supportActionBar?.setTitle(screenItem.titleStringId)
+            return true
+        }
+        return false
     }
 }
---
+++ app/src/main/java/com/example/android/barebone/ui/home/MainScreen.kt
@@ -0,0 +1,50 @@
+package com.example.android.barebone.ui.home
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.IdRes
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+
+
+import com.example.android.barebone.ui.home.homefragments.FragmentA
+import com.example.android.barebone.ui.home.homefragments.FragmentB
+import com.example.android.barebone.ui.home.homefragments.FragmentC
+
+/**
+ * Screens available for display in the main screen, with their respective titles,
+ * icons, and menu item IDs and fragments.
+ */
+enum class MainScreen(
+    @IdRes val menuItemId: Int,
+    @DrawableRes val menuItemIconId: Int,
+    @StringRes val titleStringId: Int,
+    val fragment: Fragment
+) {
+    HOME(
+        com.example.android.barebone.R.id.navigation_home,
+        com.example.android.barebone.R.drawable.ic_home_black_24dp,
+        com.example.android.barebone.R.string.title_home,
+        FragmentA()
+    ),
+    DASHBOARD(
+        com.example.android.barebone.R.id.navigation_dashboard,
+        com.example.android.barebone.R.drawable.ic_dashboard_black_24dp,
+        com.example.android.barebone.R.string.title_dashboard,
+        FragmentB()
+    ),
+    NOTIFICATION(
+        com.example.android.barebone.R.id.navigation_notifications,
+        com.example.android.barebone.R.drawable.ic_notifications_black_24dp,
+        com.example.android.barebone.R.string.title_notifications,
+        FragmentC()
+    )
+}
+
+fun getMainScreenForMenuItem(menuItemId: Int): MainScreen? {
+    for (mainScreen in MainScreen.values()) {
+        if (mainScreen.menuItemId == menuItemId) {
+            return mainScreen
+        }
+    }
+    return null
+}
